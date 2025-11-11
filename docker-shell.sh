0e1d7aeaa18577d2dd76b6c47719070072203245
Bhargav Kosaraju <kosaraju_b@yahoo.com>
2025-11-11 07:25:08 -0800

    dataset updates...

--- docker-shell.sh
+++ docker-shell.sh
@@ -4,7 +4,7 @@ set -e
 
 export BASE_DIR=$(pwd)
 export SECRETS_DIR=$(pwd)/../secrets/
-export GCS_BUCKET_NAME="cheese-app-data-versioning"
+export GCS_BUCKET_NAME="bkos-data-versioning"
 export GCP_PROJECT="ac215-project"
 export GCP_ZONE="us-central1-a"
 export GOOGLE_APPLICATION_CREDENTIALS="/secrets/data-service-account.json"
@@ -13,15 +13,16 @@ export GOOGLE_APPLICATION_CREDENTIALS="/secrets/data-service-account.json"
 echo "Building image"
 docker build -t data-version-cli -f Dockerfile .
 
+#--privileged \
+#-v ~/.gitconfig:/etc/gitconfig \
+#-e GCP_PROJECT=$GCP_PROJECT \
+#-e GCP_ZONE=$GCP_ZONE \
+
 echo "Running container"
 docker run --rm --name data-version-cli -ti \
---privileged \
 --cap-add SYS_ADMIN \
 --device /dev/fuse \
 -v "$BASE_DIR":/app \
 -v "$SECRETS_DIR":/secrets \
--v ~/.gitconfig:/etc/gitconfig \
 -e GOOGLE_APPLICATION_CREDENTIALS=$GOOGLE_APPLICATION_CREDENTIALS \
--e GCP_PROJECT=$GCP_PROJECT \
--e GCP_ZONE=$GCP_ZONE \
 -e GCS_BUCKET_NAME=$GCS_BUCKET_NAME data-version-cli
